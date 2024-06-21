f40d9c3cea7fa7e419e631b735a81e77b6c78276
Akshay Vs <me.akshay.vs@gmail.com>
2024-06-21 13:32:46 +0530

    fix: fixed invalid entrypoint lib/main.ts -> components/index.ts

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   build: {
     // output entry settings
     lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
+      entry: resolve(__dirname, 'components/index.ts'),
       name: 'artimis',
       fileName: 'artimis',
     },

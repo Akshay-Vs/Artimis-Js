6f43498fe657483f55386ffecff1900fe2743402
Akshay Vs <me.akshay.vs@gmail.com>
2024-06-30 20:22:17 +0530

    refactor(newArrivals): refactored to use map to render products

--- examples/ecommerce-demo-1/components/specific/Home/NewArrivals/NewArrivals.tsx
+++ examples/ecommerce-demo-1/components/specific/Home/NewArrivals/NewArrivals.tsx
@@ -33,6 +33,33 @@ const NewArrivals = () => {
     }
   };
 
+  const products = [
+    {
+      src: 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f',
+      category: 'women',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1527010154944-f2241763d806',
+      category: 'Men',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1511852365831-4c1b2b2b1325',
+      category: 'Kids',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1599309329365-0a9ed45a1da3',
+      category: 'women',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1485230895905-ec40ba36b9bc',
+      category: 'women',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1603986153795-d142dbbe0ae0',
+      category: 'women',
+    },
+  ];
+
   return (
     <section className="flex flex-col gap-8">
       <div className="flex-center-between">
@@ -55,32 +82,16 @@ const NewArrivals = () => {
         className="flex gap-4 overflow-x-scroll whitespace-nowrap"
         ref={scrollRef}
       >
-        <ProductCard
-          className="min-w-[50vw] max-w-[50vw]"
-          src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f"
-          category="women"
-        />
-        <ProductCard
-          src="https://images.unsplash.com/photo-1527010154944-f2241763d806"
-          category="Men"
-        />
-
-        <ProductCard
-          src="https://images.unsplash.com/photo-1511852365831-4c1b2b2b1325"
-          category="Kids"
-        />
-        <ProductCard
-          src="https://images.unsplash.com/photo-1599309329365-0a9ed45a1da3"
-          category="women"
-        />
-        <ProductCard
-          src="https://images.unsplash.com/photo-1485230895905-ec40ba36b9bc"
-          category="women"
-        />
-        <ProductCard
-          src="https://images.unsplash.com/photo-1603986153795-d142dbbe0ae0"
-          category="women"
-        />
+        {products.map((product, index) => {
+          return (
+            <ProductCard
+              key={index}
+              className={`${index === 0 ? 'min-w-[50vw] max-w-[50vw]' : ''} cursor-pointer`}
+              category={product.category}
+              src={product.src}
+            />
+          );
+        })}
       </div>
     </section>
   );

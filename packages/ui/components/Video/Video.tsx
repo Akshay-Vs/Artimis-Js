281b5cdc64e08f4ffbeb1e7f4ab11613f2dd9380
Akshay Vs <me.akshay.vs@gmail.com>
2024-07-04 20:31:44 +0530

    feat(@artimisjs/ui): added circle & video components

---
+++ packages/ui/components/Video/Video.tsx
@@ -0,0 +1,23 @@
+// TODO: Add cva variants
+import React, { HTMLProps } from 'react';
+
+type VideoProps = HTMLProps<HTMLVideoElement> & {};
+
+const Video = ({ src }: VideoProps) => {
+  return (
+    <video
+      width="1080"
+      height="1080"
+      loop
+      muted
+      autoPlay
+      preload="none"
+      className={`object-cover object-center bg-gray-200`}
+    >
+      <source src={src} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  );
+};
+
+export default Video;

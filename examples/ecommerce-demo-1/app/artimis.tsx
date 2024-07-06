04f66f49d52d5e0c7a86802532d8cb4b7dfddec4
Akshay Vs <me.akshay.vs@gmail.com>
2024-07-06 13:22:22 +0530

    fix: fixed breaking changes @artimisjs/ui 0.1.7

--- examples/ecommerce-demo-1/app/artimis.tsx
+++ examples/ecommerce-demo-1/app/artimis.tsx
@@ -2,9 +2,8 @@
 import { Header } from '@/components/layout';
 import SmoothScroll from '@/components/layout/SmoothScroll/SmoothScroll';
 import { ArtimisCore } from '@artimisjs/core';
-import React, { PropsWithChildren } from 'react';
 
-const Artimis = ({ children }: PropsWithChildren) => {
+const Artimis = ({ children }: any) => {
   return (
     <ArtimisCore dataProvider={{}} styleProvider={{}}>
       <SmoothScroll>

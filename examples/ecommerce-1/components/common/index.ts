4d038e3fbe850ceed882af8423979af2c1c05ce9
Akshay Vs <me.akshay.vs@gmail.com>
2024-07-08 23:33:37 +0530

    fix: minor change - fixed typo

--- examples/ecommerce-1/components/common/index.ts
+++ examples/ecommerce-1/components/common/index.ts
@@ -2,12 +2,10 @@ import Hamburger from "./Icons/Hamburger";
 import Input from "./Input/Input";
 import ProductCard from "./Card/ProductCard";
 import ProductCardFull from "./Card/ProductCardFull";
-import Video from "./Video/Video";
 
 export {
   Hamburger,
   Input,
   ProductCard,
   ProductCardFull,
-  Video
 }

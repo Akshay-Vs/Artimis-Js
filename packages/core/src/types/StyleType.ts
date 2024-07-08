c196442fab33bf2a20c771027814fdf3aaea9133
Akshay Vs <me.akshay.vs@gmail.com>
2024-07-08 21:58:42 +0530

    feat(core): added token types

--- packages/core/src/types/StyleType.ts
+++ packages/core/src/types/StyleType.ts
@@ -1,14 +1,21 @@
+type colorsType = {
+  primaryColor?: string,
+  secondaryColor?: string,
+  successColor?: string,
+  accentColor?: string,
+  backgroundColor?: string,
+  fontColor?: string,
+  warningColor?: string,
+  errorColor?: string,
+  infoColor?: string,
+}
+
 export type StyleType = {
   colors?: {
-    primaryColor?: string,
-    secondaryColor?: string,
-    successColor?: string,
-    accentColor?: string,
-    backgroundColor?: string,
-    fontColor?: string,
-    warningColor?: string,
-    errorColor?: string,
-    infoColor?: string,
+    theme?: {
+      dark: colorsType,
+      light: colorsType,
+    }
   },
   tokens?: {
     radius?: 'none' | 'sm' | 'md' | 'lg' | '|xl' | '2xl' | '3xl',
